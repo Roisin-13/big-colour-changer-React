54431f932996afd4c5df28ad9fa97f7aad1a9056
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-08-02 14:52:27 +0100

    started new app - trying to seperate concerns

---
+++ src/components/header.js
@@ -0,0 +1,58 @@
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    AppHeader : {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white',
+    },
+ heady1 : {
+    display: 'block',
+    backgroundColor: 'white',
+    textAlign: 'center',
+    fontFamily: '"American Typewriter", serif',
+    fontSize: 25,
+    color: 'grey',
+    width: '100 %',
+    height: 150,
+    padding: 50,
+},
+navBtn : {
+    display: 'inline',
+    listStyle: 'none',
+    margin: '20px 0px 20px 0px',
+    height: 'auto',
+    fontFamily: '"American Typewriter", serif',
+    fontWeight: 'bolder',
+    fontSize: 15,
+    padding: 10,
+    border: '1px solid lightgrey',
+    "& a ": {
+        color: 'grey',
+        textDecoration: 'none',
+    },
+},
+
+});
+
+
+function Header() {
+    const classes = useStyles();
+    return (
+        <header class={classes.heady1}>
+            <h1>Colour Changer</h1>
+            <ul>
+                <li class={classes.navBtn}><a href="#">HTML colours</a></li>
+                <li class={classes.navBtn}><a href="#">RGB colours</a></li>
+                <li class={classes.navBtn}><a href="#">HEX colours</a></li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
